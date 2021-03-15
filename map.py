bc49d0466ce746f87ac650dcf6c06d4c252ee38b
Marat Satskevich <satskevich.mv@phystech.edu>
2021-03-15 20:25:43 +0300

    Created class Map

--- map.py
+++ map.py
@@ -1,2 +1,23 @@
 class Map:
-    pass
+    def __init__(self, game):
+        self.game = game
+        self.squads = []
+        self.fortresses = []
+        self.protagonist = Player()
+        self.antagonist = Player()
+        self.turn = -1
+
+        if game.network.is_host():
+            self.generate_map()
+            self.send_state()
+        else:
+            self.get_state()
+
+    def send_state(self):
+        pass
+
+    def get_state(self):
+        pass
+
+    def generate_map(self):
+        pass
