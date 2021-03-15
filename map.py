d6070e06b173bb1847008ed781d5ea198e4c63a7
Serge Sargsyan <sargsian.st@phystech.edu>
2021-03-15 20:38:55 +0300

    Moved code from one file to another

--- map.py
+++ map.py
@@ -1,2 +1,25 @@
-class Map:
-    pass
+class Player:
+    def __init__(self):
+        self.gold = 0
+
+
+class Fortress:
+    def __init__(self, game, x, y):
+        self.game = game
+        self.garisson = None
+        self.guest = None
+        self.shop = []
+        self.master = None
+        self.x = x
+        self.y = y
+        self.generate_shop()
+
+    def open_base_menu(self):
+        pass
+
+    def change_master(self, master):
+        self.master = master
+
+    def generate_shop(self):
+        pass
+
