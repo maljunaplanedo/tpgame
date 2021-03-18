1c3ddcf6be2988d860cc281d72440a5cc901e885
Marat Satskevich <satskevich.mv@phystech.edu>
2021-03-18 20:12:34 +0300

    Many updates

--- game.py
+++ game.py
@@ -5,17 +5,17 @@ from graphics import Window
 
 class Game:
     def __init__(self):
-        # self.map = Map(self)
-        # self.network = Network()
+        self.map = Map(self)
+        self.network = Network()
         self.window = Window(self)
         self.main_loop_running = False
 
     def run(self):
-        # self.network.check_file_data()
+        self.network.check_file_data()
         self.main_loop_running = True
         while self.main_loop_running:
             self.window.iteration()
-            # self.network.iteration()
+            self.network.iteration()
 
     def stop(self):
         self.main_loop_running = False
